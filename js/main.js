12cd9fdf855f7558772aa010fe56810213cc737d
Vadim Losenkov <vprog0729@gmail.com>
2021-12-30 12:25:24 +0300

    end 4 pages

--- js/main.js
+++ js/main.js
@@ -46,9 +46,70 @@ $(function() {
 
 // ПЕРЕНЕСТИ ВСЕ ЧТО ВЫШЕ В index.js 
 
+class Tabson {
+
+  constructor(wrapper) {
+
+    this.wrapper = wrapper
+
+    this.$menu = null
+    this.$items = null
+
+    this.listener()
+  }
+
+  listener() {
+    document.addEventListener('click', event => {
+      const $wraper = event.target.closest(this.wrapper)
+      const id = event.target.closest('[data-id]').dataset.id
+      if ($wraper && id) {
+        this.$menu = $wraper.querySelector('[data-tabson="menu"]')
+        this.$items = $wraper.querySelector('[data-tabson="items"]')
+
+        this.opener(id)
+      }
+    })
+  }
+
+  opener(id) {
+    this.close()
+
+    this.$menu.querySelector(`[data-id="${id}"]`).classList.add('open')
+    this.$items.querySelector(`[data-item="${id}"]`).classList.add('open')
+  }
+
+  close() {
+    const menuList = this.$menu.querySelectorAll('[data-id]')
+    const itemsList = this.$items.querySelectorAll('[data-item]')
+
+    menuList.forEach($el => $el.classList.remove('open'))
+    itemsList.forEach($el => $el.classList.remove('open'))
+  }
+}
+new Tabson('.courses__inner')
+
+
 $(".reviews__item-rate").rateYo({
   readOnly: true,
   ratedFill: "#fda700",
   normalFill: "#c4c4c4",
   starWidth: "12px"
 })
+
+const $progressBar = $('[data-progress]')
+$progressBar.each(function(index) {
+  $(this).css({
+    width: `${$(this).attr('data-progress')}%`,
+    backgroundColor: progressBarColor($(this).attr('data-progress'))
+  })
+})
+
+function progressBarColor(num) {
+  if (num <= 40) {
+    return 'red'
+  } else if (num <= 80) {
+    return '#ffca22'
+  } else if (num <= 100) {
+    return '#93E211'
+  }
+}
